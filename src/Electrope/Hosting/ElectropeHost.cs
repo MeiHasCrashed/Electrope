c41f8f0ec113a8f883e316a4385d77bd2f1f6b75
MeiHasCrashed <meihascrashed@outlook.com>
2025-10-01 21:44:25 +0200

    Added initial ElectropeHost.

---
+++ src/Electrope/Hosting/ElectropeHost.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2025 MeiHasCrashed
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Electrope.IoC;
+using Electrope.Logging.Dalamud;
+using Electrope.Logging.EventTracing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Electrope.Hosting;
+
+public static class ElectropeHost
+{
+    public static IHostApplicationBuilder CreateDefaultBuilder(ElectropeHostOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        var environmentName = "Production";
+        if (options.PluginInterface.IsDev)
+        {
+            environmentName = "Development";
+        }
+        else if (options.PluginInterface.IsTesting)
+        {
+            environmentName = "Staging";
+        }
+
+        var builder = Host.CreateEmptyApplicationBuilder(new HostApplicationBuilderSettings
+        {
+            ApplicationName = options.PluginName ?? options.PluginInterface.InternalName,
+            ContentRootPath = options.PluginInterface.ConfigDirectory.FullName,
+            DisableDefaults = true,
+            EnvironmentName = environmentName
+        });
+
+        var pluginLog = DalamudServiceHelper<IPluginLog>.GetService(options.PluginInterface);
+
+        builder.Logging
+            .ClearProviders()
+            .SetMinimumLevel(LogLevel.Trace)
+            .AddDalamudLogging(pluginLog);
+
+        if (options.EventTracingGuid is not null)
+        {
+            var guid = Guid.Parse(options.EventTracingGuid);
+            builder.Logging.AddEventTracingLogger(guid);
+        }
+
+        builder.Services
+            .AddSingleton(options.PluginInterface)
+            .AddSingleton(pluginLog);
+
+        return builder;
+    }
+}
---
+++ src/Electrope/Hosting/ElectropeHostOptions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2025 MeiHasCrashed
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace Electrope.Hosting;
+
+public class ElectropeHostOptions
+{
+    /// <summary>
+    /// The name of your plugin. Will be used for the host ApplicationName.
+    /// </summary>
+    public string? PluginName { get; init; }
+
+    /// <summary>
+    /// Dalamud's plugin interface. Required to access Dalamud's IoC system.
+    /// </summary>
+    public required IDalamudPluginInterface PluginInterface { get; init; }
+
+    /// <summary>
+    /// If you want to use the EventTracing logging provider, set this to a custom GUID.
+    /// You can use the provided GUID to capture ETW logs in string format (sent with EventWriteString).
+    /// <remarks>This only works on OSPlatform.Windows, and will be a no-op everywhere else.</remarks>
+    /// </summary>
+    public string? EventTracingGuid { get; init; }
+}
